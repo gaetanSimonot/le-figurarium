f9fabd6cc4695f9f7d2469c0f0914d9c839ad7af
GaetanSimonot <gaetan.simonot@gmail.com>
2025-10-02 16:55:57 +0200

    Remove out/ folder from Git tracking

--- out/_next/static/chunks/9c685a384c1c9fe3.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/d4f85c85b812c475.js",
-  "static/chunks/turbopack-a51d2e1146a79ea9.js"
-])
--- out/_next/static/chunks/9d981071e7a0895e.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/d4f85c85b812c475.js",
-  "static/chunks/turbopack-cb0356c51093de85.js"
-])
